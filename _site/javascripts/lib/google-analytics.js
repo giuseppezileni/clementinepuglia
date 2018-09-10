7264ec824c42357186fab9b656052ec710c00669
Giuseppe Zileni <giuseppe.zileni@gmail.com>
2018-09-10 16:57:02 +0200

    www.clementine.puglia.it

--- _site/javascripts/lib/google-analytics.js
+++ _site/javascripts/lib/google-analytics.js
@@ -3,5 +3,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-9970885-25', 'auto');
+  ga('create', 'UA-73411437-1', 'auto');
   ga('send', 'pageview');
